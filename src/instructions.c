82c842a75f2437d9b668bcafe100575aa8138ce2
Santito <santito@santito.es>
2024-01-05 13:40:05 +0100

    [Setup] Tooling

--- src/instructions.c
+++ src/instructions.c
@@ -6,7 +6,7 @@
 /*   By: sguzman <sguzman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 10:27:28 by sguzman           #+#    #+#             */
-/*   Updated: 2024/01/05 11:34:07 by sguzman          ###   ########.fr       */
+/*   Updated: 2024/01/05 13:38:01 by sguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,38 +24,6 @@ static void	pb_command(t_stack **a, t_stack **b)
 	pb(a, b);
 	if (!*b)
 		wipe(a, b);
-}
-
-void	printStacks(t_stack *stackA, t_stack *stackB)
-{
-	while (stackA || stackB)
-	{
-		if (stackA)
-		{
-			printf("%d ", stackA->value);
-			stackA = stackA->next;
-		}
-		else
-			printf("  ");
-		if (stackB)
-		{
-			printf("%d ", stackB->value);
-			stackB = stackB->next;
-		}
-		else
-			printf("  ");
-		printf("\n");
-	}
-	printf("_ _\n");
-	printf("a b\n");
-	printf("\n");
-}
-
-void	perform_and_log(t_stack **a, t_stack **b, t_operation instruction)
-{
-	ft_putstr(instruction.order, STDOUT_FILENO);
-	instruction.command(a, b);
-	// printStacks(*a, *b);
 }
 
 t_operation	*init_instructions(void)
--- src/output.c
+++ src/output.c
@@ -6,7 +6,7 @@
 /*   By: sguzman <sguzman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 09:58:28 by sguzman           #+#    #+#             */
-/*   Updated: 2024/01/05 10:01:43 by sguzman          ###   ########.fr       */
+/*   Updated: 2024/01/05 13:37:59 by sguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,36 @@ void	display_colored(char *s, char *color, int fd)
 	ft_putstr(color, fd);
 	ft_putstr(s, fd);
 	ft_putstr(RESET, fd);
+}
+
+void	printStacks(t_stack *stackA, t_stack *stackB)
+{
+	while (stackA || stackB)
+	{
+		if (stackA)
+		{
+			printf("%d ", stackA->value);
+			stackA = stackA->next;
+		}
+		else
+			printf("  ");
+		if (stackB)
+		{
+			printf("%d ", stackB->value);
+			stackB = stackB->next;
+		}
+		else
+			printf("  ");
+		printf("\n");
+	}
+	printf("_ _\n");
+	printf("a b\n");
+	printf("\n");
+}
+
+void	perform_and_log(t_stack **a, t_stack **b, t_operation instruction)
+{
+	ft_putstr(instruction.order, STDOUT_FILENO);
+	instruction.command(a, b);
+	// printStacks(*a, *b);
 }
