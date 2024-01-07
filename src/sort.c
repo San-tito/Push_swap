44e27957e27460bcd3b80cdc48d24a690e31edd8
Santito <santito@santito.es>
2024-01-07 20:03:29 +0100

    [Insertion] <3

--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,7 @@
 /*   By: sguzman <sguzman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 15:13:33 by sguzman           #+#    #+#             */
-/*   Updated: 2024/01/07 15:42:50 by sguzman          ###   ########.fr       */
+/*   Updated: 2024/01/07 19:59:51 by sguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,21 +29,47 @@ static int	get_index(t_stack *stack, t_stack *wanted)
 	return (index);
 }
 
+t_stack	*find_min(t_stack *stack)
+{
+	t_stack	*current;
+	t_stack	*min;
+
+	current = stack;
+	min = current;
+	while (current)
+	{
+		if (current->value < min->value)
+			min = current;
+		current = current->next;
+	}
+	return (min);
+}
+
+t_stack	*find_max(t_stack *stack)
+{
+	t_stack	*max;
+	t_stack	*current;
+
+	max = stack;
+	current = (*stack).next;
+	while (current)
+	{
+		if ((*stack).value < (*max).value)
+			max = current;
+		current = (*current).next;
+	}
+	return (max);
+}
+
 void	insertion_sort(t_stack **a, t_stack **b)
 {
 	t_stack	*min;
-	t_stack	*current;
 
+	// int		chunk_size;
+	// chunk_size = 20 + ((size(a) - 100) / 16);
 	while (!stack_is_sorted(*a))
 	{
-		min = *a;
-		current = (**a).next;
-		while (current)
-		{
-			if ((*current).value < (*min).value)
-				min = current;
-			current = (*current).next;
-		}
+		min = find_min(*a);
 		while (*a != min)
 		{
 			if ((size(a) / 2) < get_index(*a, min))
